a9aa350bf94038e1c3869d4dfefad21ff574786a
Shreyas <shreyasacharya8@gmail.com>
2024-02-12 01:18:03 +0530

    Valid Parenthesis problem #20

---
+++ 20-ValidParentheis.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        c=len(s)
+        i=0
+        j=1
+        b=True
+        while c>0:
+            if s[i]=="(":
+                if s[j]==")":
+                    b=True
+                else:
+                    return False
+            if s[i]=="[":
+                if s[j]=="]":
+                    b=True
+                else:
+                    return False
+            if s[i]=="{":
+                if s[j]=="}":
+                    b=True
+                else:
+                    return False
+            
+            i+=2
+            j+=2
+            c-=2
+        return True
